21b086ad1d087cba8b7586efe776bdb9c27c94dd
Matthias Beitl <cssence@gmail.com>
2018-07-03 12:57:28 +0200

    /notepad with distinct view/edit mode (so we can have clickable links)

--- assets/notepad/app.js
+++ assets/notepad/app.js
@@ -7,12 +7,25 @@
 		return;
 	}
 	var TEXT_KEY = location.pathname;
-	var text = window.localStorage.getItem(TEXT_KEY);
-	document.querySelector("textarea").value = text;
-	document.querySelector(".action--select-all").addEventListener("click", function (event) {
-		document.querySelector("textarea").select();
+	var textarea = document.querySelector("textarea");
+	textarea.value = window.localStorage.getItem(TEXT_KEY);
+	var update = function () {
+		var pre = document.querySelector("pre");
+		pre.innerHTML = textarea.value.replace(/(\b(https?|mailto|tel):\/\/[^\s]*)/g, function (url) {
+			return "<a target=\"_blank\" href=\"" + url + "\">" + url + "</a>";
+		});
+	};
+	document.querySelector(".action--toggle").addEventListener("click", function (event) {
+		var body = document.querySelector("body");
+		body.classList.toggle("edit");
+		if (body.classList.contains("edit")) {
+			textarea.focus();
+		} else {
+			update();
+		}
 	});
-	document.querySelector("textarea").addEventListener("blur", function (event) {
+	textarea.addEventListener("blur", function (event) {
 		window.localStorage.setItem(TEXT_KEY, event.target.value);
 	});
+	update();
 }(window, document, navigator));
