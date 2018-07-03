561477c0406a321d2f8f1cb5a93156f2d959c934
Matthias Beitl <cssence@gmail.com>
2018-07-03 12:53:46 +0200

    serviceworke.rs Cache-and-Update for same-origin

--- assets/sw.js
+++ assets/sw.js
@@ -2,7 +2,7 @@
 (() => {
 	"use strict";
 
-	const assetCacheName = "assets-v1.0";
+	const assetCacheName = "assets-v1.1";
 
 	self.addEventListener("install", event => {
 		event.waitUntil(
@@ -15,9 +15,20 @@
 		const request = event.request;
 		const url = new URL(request.url);
 		if (url.origin === location.origin) {
+			// respond from cache
 			event.respondWith(
-				caches.open(assetCacheName).then(cache => {
-					return cache.match(request).then(response => response || fetch(request));
+				caches.open(assetCacheName).then(function (cache) {
+					return cache.match(request).then(function (matching) {
+						return matching || Promise.reject("no-match");
+					});
+				})
+			);
+			// check if we need to store an updated version
+			event.waitUntil(
+				caches.open(assetCacheName).then(function (cache) {
+					return fetch(request).then(function (response) {
+						return cache.put(request, response);
+					});
 				})
 			);
 		} else {
